bb6bf92f02cd2ea4ebbcec2361647df240728878
edsonabelha <edson.cauxeiro87@gmail.com>
2020-07-22 02:56:21 +0100

    Criação do Card Promotion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PromotionCard from './components/Promotion/Card/Card';
 
-function App() {
+/*  
+ {
+      "id": 1,
+      "title": "Kit Notebook Acer Aspire 3 + Mochila Green, A315-41-R790, AMD Ryzen 3 2200U Dual Core",
+      "price": 1799,
+      "url": "https://www.amazon.com.br",
+      "imageUrl": "https://cdn.gatry.com/gatry-static/promocao/imagem_url/2631517face1861bc4f46ae154d387de.png",
+      "comments": [
+          {
+            "id": 1,
+            "comment": "TELA HD",
+            "promotionId": 1
+          }
+      ]
+  }
+*/
+const  App = () => {
+
+  const promotion = {
+      "id": 1,
+      "title": "Kit Notebook Acer Aspire 3 + Mochila Green, A315-41-R790, AMD Ryzen 3 2200U Dual Core",
+      "price": 1799,
+      "url": "https://www.amazon.com.br",
+      "imageUrl": "https://cdn.gatry.com/gatry-static/promocao/imagem_url/2631517face1861bc4f46ae154d387de.png",
+      "comments": [
+          {
+            "id": 1,
+            "comment": "TELA HD",
+            "promotionId": 1
+          }
+      ]
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+      maxWidth: 800,
+      margin: '30px auto',
+    }}>
+      <PromotionCard promotion={promotion} />
     </div>
   );
 }
